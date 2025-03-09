db808fbd06d01b10d76c8bbcf3d81e193b1ed418
robieli <robieli@umich.edu>
2025-03-09 19:08:30 -0400

    Replaced Form with LLM query container

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,91 +1,58 @@
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
 import "./form.css";
 
 function Form() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [llmInput, setLlmInput] = useState("");
+  const [llmOutput, setLlmOutput] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit() {
+    if (llmInput === "") {
+      return "No input";
+    }
+    try {
+      setLoading(true);
+      const url =
+        "http://127.0.0.1:5000/llm?query=" +
+        llmInput.toString() +
+        "&lang=" +
+        i18n.language.toString();
+      const returned = await (await fetch(url)).json();
+      setLlmOutput(returned);
+      setLoading(false);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  function handleClear() {
+    setLlmOutput("");
+  }
+
+  function handleText(e) {
+    setLlmInput(document.getElementById("input").textContent);
+  }
 
   return (
-    <div className="container-tri">
-      <form>
-        <div className="container-name">
-          <div>
-            <label for="fname">{t("form.name.first")}:</label>
-            <br />
-            <input type="text" id="fname" name="fname" />
-          </div>
-          <div>
-            <label for="lname">{t("form.name.last")}:</label>
-            <br />
-            <input type="text" id="lname" name="lname" />
-          </div>
-        </div>
-        <div>
-          <label for="paragraph" id="paragraph" name="paragraph">
-            {t("form.message")}:
-          </label>
-          <br />
-          <textarea
-            type="text"
-            id="paragraph"
-            name="paragraph"
-            placeholder={t("form.placeholder")}
-          />
-        </div>
-        <div className="container-multi">
-          <div className="container-radio">
-            <label for="option" id="option" name="option">
-              {t("form.option", { count: 0 })}:
-            </label>
-            <br />
-            <div>
-              <input type="radio" id="one" name="option" value="one" />
-              <label id="one" for="one">
-                {t("form.option", { count: 1, num: 1 })}
-              </label>
-            </div>
-            <div>
-              <input type="radio" id="two" name="option" value="two" />
-              <label id="two" for="two">
-                {t("form.option", { count: 1, num: 2 })}
-              </label>
-            </div>
-            <div>
-              <input type="radio" id="three" name="option" value="three" />
-              <label id="three" for="three">
-                {t("form.option", { count: 1, num: 3 })}
-              </label>
-            </div>
-          </div>
-          <div className="container-checkbox">
-            <label for="option" id="option" name="option">
-              {t("form.option", { count: 0 })}:
-            </label>
-            <br />
-            <div>
-              <input type="checkbox" id="one" name="option" value="one" />
-              <label id="one" for="one">
-                {t("form.option", { count: 1, num: 1 })}
-              </label>
-            </div>
-            <div>
-              <input type="checkbox" id="two" name="option" value="two" />
-              <label id="two" for="two">
-                {t("form.option", { count: 1, num: 2 })}
-              </label>
-            </div>
-            <div>
-              <input type="checkbox" id="three" name="option" value="three" />
-              <label id="three" for="three">
-                {t("form.option", { count: 1, num: 3 })}
-              </label>
-            </div>
-          </div>
-        </div>
-        <div className="buttons">
-          <button className="form-button blue">{t("common.submit")}</button>
-          <button className="form-button">{t("common.cancel")}</button>
-        </div>
-      </form>
+    <div className="container-tri form">
+      <h4>{t("form.header")}:</h4>
+      <div contentEditable onInput={handleText} id="input"></div>
+      <div className="submit-container">
+        <button
+          className="form-button"
+          onClick={handleSubmit}
+          disabled={loading}
+        >
+          {t("common.submit")}
+        </button>
+        <button className="form-button" onClick={handleClear}>
+          {t("form.clear")}
+        </button>
+        <p className="loading">{loading ? "Loading..." : ""}</p>
+      </div>
+      <div className="output">{llmOutput}</div>
     </div>
   );
 }
